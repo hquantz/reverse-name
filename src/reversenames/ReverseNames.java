8a05fc656c22c20d34328c9c394b763a98edffb8
hquantz <hunter.quantz9302@mail.davistech.edu>
2022-01-05 18:52:06 -0700

    Added user input and the first portion of the name reversal

--- src/reversenames/ReverseNames.java
+++ src/reversenames/ReverseNames.java
@@ -13,7 +13,23 @@ public class ReverseNames {
 
     
     public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        String name; //stores the name entered by the user
+        String reverseName = ""; //stores the name when it has been reversed
+        int indexOfSpace;//stores the index of the space
         
+        System.out.println("Reverse Name by Hunter Quantz\n");
+        System.out.print("What is your name? ");
+        name = stdIn.nextLine();
+        System.out.println(name);
+        
+        indexOfSpace = name.indexOf(" ");
+        
+        for(int i = indexOfSpace - 1; i >= 0; i--){
+            reverseName += name.charAt(i);
+        }
+        
+        System.out.println(reverseName);
     }//end main
 
 }//end reversenames class
