c557e2fa13f923ef6ebaa995e60d176f0eb46349
hquantz <hunter.quantz9302@mail.davistech.edu>
2022-01-05 18:36:17 -0700

    created initial files

---
+++ src/reversenames/ReverseNames.java
@@ -0,0 +1,19 @@
+/*
+ * ReverseNames.java
+ * Hunter Quantz
+ * 
+ * This program takes user input for a name and returns it with 
+ * the letters in the first and last names reversed.
+ */
+package reversenames;
+
+import java.util.Scanner;
+
+public class ReverseNames {
+
+    
+    public static void main(String[] args) {
+        
+    }//end main
+
+}//end reversenames class
