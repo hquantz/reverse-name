0c05a4e5ca6262b5f726c17b20fbdc9a5c908f99
hquantz <hunter.quantz9302@mail.davistech.edu>
2022-01-05 19:19:23 -0700

    Capitalized first letter in each name

--- src/reversenames/ReverseNames.java
+++ src/reversenames/ReverseNames.java
@@ -11,30 +11,44 @@ import java.util.Scanner;
 
 public class ReverseNames {
 
-    
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         String name; //stores the name entered by the user
         String reverseName = ""; //stores the name when it has been reversed
         int indexOfSpace;//stores the index of the space
         int nameLength; //stores the length of the name
-        
+        String letterToCapitalize = ""; //stores the letter to capitalize
+
         System.out.println("Reverse Name by Hunter Quantz\n");
         System.out.print("What is your name? ");
         name = stdIn.nextLine();
         nameLength = name.length();
-        
+        name = name.toLowerCase();
+
         indexOfSpace = name.indexOf(" ");
-        
-        for(int i = indexOfSpace - 1; i >= 0; i--){
-            reverseName += name.charAt(i);
+
+        for (int i = indexOfSpace - 1; i >= 0; i--) {
+            if (i == indexOfSpace - 1) {
+                letterToCapitalize += name.charAt(i);
+                reverseName += letterToCapitalize.toUpperCase();
+                letterToCapitalize = "";
+            } else {
+                reverseName += name.charAt(i);
+            }
+
         }
-        
+
         reverseName += " ";
-        
-        for(int i = nameLength - 1; i > indexOfSpace; i--){
-            reverseName += name.charAt(i);
+
+        for (int i = nameLength - 1; i > indexOfSpace; i--) {
+            if (i == nameLength - 1) {
+                letterToCapitalize += name.charAt(i);
+                reverseName += letterToCapitalize.toUpperCase();
+            } else {
+                reverseName += name.charAt(i);
+            }
         }
+
         System.out.println(reverseName);
     }//end main
 
